8a81457a4e12729bb0b3fc3042e059e9fb9671b6
Jane Philipps <jane.philipps@gmail.com>
2015-04-09 14:16:01 -0700

    Add User model, update config.json db

---
+++ migrations/20150409141206-create-user.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      email_address: {
+        type: DataTypes.STRING
+      },
+      password: {
+        type: DataTypes.STRING
+      },
+      zip_code: {
+        type: DataTypes.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    }).done(done);
+  },
+  down: function(migration, DataTypes, done) {
+    migration.dropTable("Users").done(done);
+  }
+};
