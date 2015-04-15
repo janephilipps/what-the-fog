381bd28f178d0b345346ad7807210edfc7021a71
Jane Philipps <jane.philipps@gmail.com>
2015-04-15 15:31:12 -0700

    Adding fog logic

--- public/js/style.js
+++ public/js/style.js
@@ -3,4 +3,17 @@ window.onload = function() {
 	var opacity = el.innerHTML;
 	console.log(el);
 	el.setAttribute("style", "background-color: rgba(128,128,128,"+opacity+")");
+
+	var fog = document.getElementById('fog');
+	var foggy = isFoggy();
+
+	if (foggy) {
+		fog.innerHTML = "FOGGY";
+	} else if (!foggy) {
+		fog.innerHTML = "NOT FOGGY";
+	} else {
+		fog.innerHTML = "WTF";
+	}
+
+	// fog.setAttribute("style", "background-color: rgba(128,")
 };
