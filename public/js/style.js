032d003e79225a80d02e07d5f23cd0e6c4b909d4
Jane Philipps <jane.philipps@gmail.com>
2015-04-15 22:06:51 -0700

    Updating fogarithm

--- public/js/style.js
+++ public/js/style.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	var el = document.getElementById('cloud');
-	var opacity = el.innerHTML;
+	var opacity = parseFloat(el.innerHTML);
 	console.log(el);
 	el.setAttribute("style", "background-color: rgba(128,128,128,"+opacity+")");
 
@@ -15,14 +15,21 @@ window.onload = function() {
 		fog.setAttribute("style", "opacity: 1")
 	}
 
-	if (opacity >= 0 && opacity < .25) {
-		cloudy.innerHTML = "A little."
+	// console.log("["+opacity+"]");
+	// console.log(typeof opacity);
+
+	if (opacity === 0) {
+		cloudy.innerHTML = "Not a cloud in the sky!";
+	} else if (opacity > 0 && opacity < .25) {
+		cloudy.innerHTML = "A little.";
 	} else if (opacity >= .25 && opacity < .5) {
-		cloudy.innerHTML = "Kinda sorta."
+		cloudy.innerHTML = "Kinda sorta.";
 	} else if (opacity >= .5 && opacity < .75) {
-		cloudy.innerHTML = "Totally."
+		cloudy.innerHTML = "Totally.";
+	} else if (opacity >= .75 && opacity < 1) {
+		cloudy.innerHTML = "Much clouds.";
 	} else {
-		cloudy.innerHTML = "Much clouds."
+		cloudy.innerHTML = "Too much clouds.";
 	}
 
 	// fog.setAttribute("style", "background-color: rgba(128,")
