9b884424708090c74a5bc6e972e7de7d83d88012
Jane Philipps <jane.philipps@gmail.com>
2015-05-18 22:05:53 -0700

    Convert tabs to spaces

--- public/js/style.js
+++ public/js/style.js
@@ -1,31 +1,31 @@
 window.onload = function() {
 
-	var el = document.getElementById('cloud');
-	var opacity = parseFloat(el.innerHTML) / 100;
-	el.setAttribute("style", "background-color: rgba(255,255,255," + opacity + ")" );
+  var el = document.getElementById('cloud');
+  var opacity = parseFloat(el.innerHTML) / 100;
+  el.setAttribute("style", "background-color: rgba(255,255,255," + opacity + ")" );
 
-	var fog = document.getElementById('fog');
+  var fog = document.getElementById('fog');
 
-	if (foggy) {
-		fog.innerHTML = "Yep.";
-		fog.setAttribute("style", "opacity: .5");
-	} else {
-		fog.innerHTML = "Nope.";
-		fog.setAttribute("style", "opacity: 1")
-	}
+  if (foggy) {
+    fog.innerHTML = "Yep.";
+    fog.setAttribute("style", "opacity: .5");
+  } else {
+    fog.innerHTML = "Nope.";
+    fog.setAttribute("style", "opacity: 1")
+  }
 
-	if (opacity === 0) {
-		cloudy.innerHTML = "Not cloudy.";
-	} else if (opacity > 0 && opacity < .25) {
-		cloudy.innerHTML = "A little cloudy.";
-	} else if (opacity >= .25 && opacity < .5) {
-		cloudy.innerHTML = "Kinda sorta cloudy.";
-	} else if (opacity >= .5 && opacity < .75) {
-		cloudy.innerHTML = "Totally cloudy.";
-	} else if (opacity >= .75 && opacity < 1) {
-		cloudy.innerHTML = "Much clouds.";
-	} else {
-		cloudy.innerHTML = "Too cloudy.";
-	}
+  if (opacity === 0) {
+    cloudy.innerHTML = "Not cloudy.";
+  } else if (opacity > 0 && opacity < .25) {
+    cloudy.innerHTML = "A little cloudy.";
+  } else if (opacity >= .25 && opacity < .5) {
+    cloudy.innerHTML = "Kinda sorta cloudy.";
+  } else if (opacity >= .5 && opacity < .75) {
+    cloudy.innerHTML = "Totally cloudy.";
+  } else if (opacity >= .75 && opacity < 1) {
+    cloudy.innerHTML = "Much clouds.";
+  } else {
+    cloudy.innerHTML = "Too cloudy.";
+  }
 
 };
